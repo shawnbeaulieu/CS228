79097008e41b96ec7ab820607c346693cae26a52
ShawnBeaulieu <Shawn.Beaulieu@uvm.edu>
2019-09-04 18:29:51 -0400

    First deliverable

--- pygameWindow.py
+++ pygameWindow.py
@@ -1,10 +1,11 @@
 import pygame
+import random
 #constants.width 
 
 class PYGAME_WINDOW:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((300,100))
+        self.screen = pygame.display.set_mode((1000,500))
         
     def Prepare(self):
         self.screen.fill((255,255,255))
@@ -12,5 +13,18 @@ class PYGAME_WINDOW:
     def Reveal(self):
         pygame.display.update()
     
-    def Draw_Black_Circle(self, x,y):
-        pygame.draw.circle(self.screen, (0,0,0), (x,y), 2)
+    def Draw_Black_Circle(self, x, y, size):
+        pygame.draw.circle(self.screen, (0,0,0), (x,y), size)
+        
+    def Perturb_Circle_Position(self, x, y):
+        fourSidedDieRoll = random.randint(1,4)
+        if fourSidedDieRoll == 1:
+            x -= 1
+        elif fourSidedDieRoll == 2:
+            x += 1
+        elif fourSidedDieRoll == 3:
+            y -= 1
+        elif fourSidedDieRoll == 4:
+            y += 1
+            
+        return(x,y)
